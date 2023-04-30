687661c04578a6e8d337c3c35f31256ef7f5cc5f
christian-theriot <christian@theriot.dev>
2023-04-29 21:20:13 -0400

    alter urls so that it uses the .dev TLD rather than the .com

--- frontend/src/environments/environment.production.ts
+++ frontend/src/environments/environment.production.ts
@@ -1,6 +1,6 @@
 export const environment = {
   GOOGLE_CLIENT_ID:
     '1039686909483-v0c4tvd7d8eg5g30fovk7gjta4o253hk.apps.googleusercontent.com',
-  BACKEND_URL: 'budget.theriot.dev.com/api',
+  BACKEND_URL: 'budget.theriot.dev/api',
   ENVIRONMENT: 'production',
 };
